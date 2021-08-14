5dddb21d6ee781736094a1281055b72b8fc85983
kurtn2005 <kurtn@126.com>
2021-08-14 16:35:55 +0800

    Update random_cron.sh

--- random_cron.sh
+++ random_cron.sh
@@ -2,22 +2,22 @@
 
 sed -i '/cron:/d' .github/workflows/run.yml
 
-if [[ $(date '+%H') == 01 ]];then
-    time=$(($RANDOM%10))" 3 * * *"
-elif [[ $(date '+%H') == 03 ]];then
-    time=$(($RANDOM%10))" 5 * * *"
-elif [[ $(date '+%H') == 05 ]];then
-    time=$(($RANDOM%10))" 7 * * *"
-elif [[ $(date '+%H') == 07 ]];then
-    time=$(($RANDOM%10))" 9 * * *"
-elif [[ $(date '+%H') == 09 ]];then
-    time=$(($RANDOM%10))" 11 * * *"
-elif [[ $(date '+%H') == 11 ]];then
-    time=$(($RANDOM%5))" 13 * * *"
-elif [[ $(date '+%H') == 13 ]];then
-    time=$(($RANDOM%10))" 1 * * *"
+if [[ $(date '+%H') == 00 ]];then
+    time=$(($RANDOM%10))" 2 * * *"
+elif [[ $(date '+%H') == 02 ]];then
+    time=$(($RANDOM%10))" 4 * * *"
+elif [[ $(date '+%H') == 04 ]];then
+    time=$(($RANDOM%10))" 6 * * *"
+elif [[ $(date '+%H') == 06 ]];then
+    time=$(($RANDOM%10))" 8 * * *"
+elif [[ $(date '+%H') == 08 ]];then
+    time=$(($RANDOM%10))" 10 * * *"
+elif [[ $(date '+%H') == 10 ]];then
+    time=$(($RANDOM%5))" 12 * * *"
+elif [[ $(date '+%H') == 12 ]];then
+    time=$(($RANDOM%10))" 0 * * *"
 else
-    time=$(($RANDOM%10))" 1,3,5,7,9,11,13 * * *"
+    time=$(($RANDOM%10))" 0,2,4,6,8,10,12 * * *"
 fi
 echo 当前时间为$(date '+%H')时，计划运行时间$time
 sed -i '/schedule/a\    - cron: '$time'' .github/workflows/run.yml
