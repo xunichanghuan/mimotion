16259e7b76c1fd6dfad3d171551c0b25c740eaff
kurtn2005 <kurtn@126.com>
2021-08-27 13:00:42 +0800

    Update random_cron.sh

--- random_cron.sh
+++ random_cron.sh
@@ -13,11 +13,11 @@ elif [[ $(date '+%H') == 06 ]];then
 elif [[ $(date '+%H') == 08 ]];then
     time=$(($RANDOM%10))" 10 * * *"
 elif [[ $(date '+%H') == 10 ]];then
-    time=$(($RANDOM%10))" 13 * * *"
-elif [[ $(date '+%H') == 13 ]];then
+    time=$(($RANDOM%10))" 12 * * *"
+elif [[ $(date '+%H') == 12 ]];then
     time=$(($RANDOM%10))" 0 * * *"
 else
-    time=$(($RANDOM%10))" 0,2,4,6,8,10,13 * * *"
+    time=$(($RANDOM%10))" 0,2,4,6,8,10,12 * * *"
 fi
 echo 当前时间为$(date '+%H')时，计划运行时间$time
 sed -i '/schedule/a\    - cron: '$time'' .github/workflows/run.yml
